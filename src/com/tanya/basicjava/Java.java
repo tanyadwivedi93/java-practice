d71f56b87119bb7f4945b5e620c9afa4f989c626
tanyadwivedi93 <tanya.d@justbaat.com>
2025-06-09 14:28:09 +0530

    change2

---
+++ src/com/tanya/basicjava/Java.java
@@ -0,0 +1,84 @@
+package com.tanya.basicjava;
+import java.util.*;
+
+import javax.sql.rowset.serial.SQLOutputImpl;
+
+public class Java {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		
+		// Addition
+		//int a =2;
+		//int b =3;
+		//int sum =a+b;
+		//System.out.println(sum);
+		
+		// Subtraction
+		//int a=3;
+		//int b=2;
+		//int diff =a-b;
+		//System.out.println(diff);
+		
+		
+		//Multiplication
+		//int a=2;
+		//int b=3;
+		//int mul=a*b;
+		//System.out.println(mul);
+		
+		//Division
+		//int a=4;
+		//int b=2;
+		//int div=a/b;
+		//System.out.println(div);
+		
+		//Modular / remainder
+		int a=6;
+		int b=3;
+		int mod =a%b;
+		System.out.println(mod);
+		
+		
+		
+	
+	
+
+		
+		
+		
+		
+		//int button = sc.nextInt();
+		//	if(button == 1) {
+		//	System.out.println("Hello");
+		//}
+		//else if(button==2) {
+	    //System.out.println("Namastey");
+		//}
+		//else if(button==3){
+		//System.out.println("Bonjour");
+		//}
+		//else {
+		//System.out.println("Invalid");
+		//}
+		
+		
+		//switch(button) {
+		//case 1: System.out.println("Hello");
+		//break;
+		//case 2:System.out.println("Namastey");
+		//break;
+		//case 3:System.out.println("Bonjour");
+		//break;
+		//default:System.out.println("Invalid");
+		//}
+	
+	}
+}
+
+
+		
+		
+	
+	
+ 
